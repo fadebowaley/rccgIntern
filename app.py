f450f52209b54865f79eb197470ce010422fd2cb
Ademola Adebowale <fadebowaley@gmail.com>
2024-08-27 12:27:18 +0100

    adding of flies app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+def main():
+    # Ask the user for their name
+    name = input("Hello! What's your name?\n> ")
+    
+    # Respond with a greeting
+    print(f"Nice to meet you, {name}!")
+
+if __name__ == "__main__":
+    main()
